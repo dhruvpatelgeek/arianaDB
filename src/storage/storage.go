8554130d92c93601e2d02836c47bae0f754ed57c
Datsgood <danny.lee@mail.mcgill.ca>
2021-03-12 19:47:52 -0800

    removed debugging counter

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -249,7 +249,6 @@ func (sm *StorageModule) processKeyMigrationRequest(request protobuf.InternalMsg
 	// TODO: process key migration request
 	// extract lowerbound, upperbound, destination address & convert to appropriate
 	destination := request.GetMigrationDestinationAddress()
-	numMigrated := 0
 
 	//lowerbound := request.GetMigrationRangeLowerbound()
 	//upperbound := request.GetMigrationRangeUpperbound()
@@ -278,7 +277,6 @@ func (sm *StorageModule) processKeyMigrationRequest(request protobuf.InternalMsg
 			hashedKeyIsBounded := lowerbound.Cmp(hashedKey) == -1 && hashedKey.Cmp(upperbound) == -1
 			if hashedKeyIsBounded {
 				// migrate keys
-				numMigrated++
 				err := sm.migrateKey(key, value, destination)
 				if err != nil {
 					fmt.Println("[Storage] Failed to migrate key.", err.Error())
@@ -289,7 +287,6 @@ func (sm *StorageModule) processKeyMigrationRequest(request protobuf.InternalMsg
 			hashedKeyIsBounded := !(upperbound.Cmp(hashedKey) == -1 && hashedKey.Cmp(lowerbound) == -1)
 			if hashedKeyIsBounded {
 				// migrate keys
-				numMigrated++
 				err := sm.migrateKey(key, value, destination)
 				if err != nil {
 					fmt.Println("[Storage] Failed to migrate key.", err.Error())
@@ -297,7 +294,6 @@ func (sm *StorageModule) processKeyMigrationRequest(request protobuf.InternalMsg
 			}
 		}
 	}
-	fmt.Println("Num migrated ", numMigrated)
 	sm.kvsLock.Unlock()
 
 	// check if wrap around
