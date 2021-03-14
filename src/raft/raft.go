aa3707a386670b8e920ac015fb1b961731dfe4f6
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-03-13 22:06:05 -0800

    ALL conflits fixed [doing regression testing]

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -14,22 +14,24 @@ import (
 	"sync"
 	"time"
 )
-// raft module v2.2
-type node struct {
-	node_ip         string
-	timeoutMutex    sync.Mutex
-	leaderMutex     sync.Mutex
-	mapMutex        sync.Mutex
-	pingAck         int
-	timeout         time.Duration
-	leader_ip       string
-	voteRec         int
-	voteTermMap     map[int32]int32
-	termNum         int32
-	fromCoordinator chan protobuf.RaftPayload
-	FSM             *fsm.FSM
 
-	gmsPtr       *membership.MembershipService
+
+
+type node struct {
+	node_ip  string
+	timeoutMutex sync.Mutex
+	leaderMutex sync.Mutex
+	mapMutex sync.Mutex
+	pingAck int
+	timeout time.Duration
+	leader_ip string
+	voteRec int
+	voteTermMap map[int32]int32
+	termNum int32
+	fromCoordinator chan protobuf.RaftPayload
+	FSM *fsm.FSM
+
+	gmsPtr *membership.MembershipService
 	transportPtr *transport.TransportModule
 }
 
@@ -37,60 +39,57 @@ type node struct {
 //maximum netowkr down time is 20 seconds
 var MIN_TIME = 4
 var MAX_TIME = 18
-var REFRESH_RATE = 500 * time.Millisecond
-var NETWORK_DELAY = 1
-var GAIN = 1 * time.Second
-
+var REFRESH_RATE =500*time.Millisecond
+var NETWORK_DELAY=1
+var GAIN=1*time.Second
 //-------------------------------------
 // COLOR MESSAGES-----------------------
 var LDprint = color.New(color.FgBlue).Add(color.ReverseVideo)
-var CDprint = color.New(color.FgYellow)
-var FWprint = color.New(color.FgMagenta).Add(color.ReverseVideo)
-var Xprint = color.New(color.ReverseVideo)
-var RXprint = color.New(color.FgRed).Add(color.ReverseVideo)
-
+var CDprint=color.New(color.FgYellow)
+var FWprint=color.New(color.FgMagenta).Add(color.ReverseVideo)
+var Xprint=color.New(color.ReverseVideo)
+var RXprint=color.New(color.FgRed).Add(color.ReverseVideo)
 //-------------------------------------
 
 //INIT-----------------------------------------
 
-func New(gmsPtr *membership.MembershipService,
+func New(	gmsPtr *membership.MembershipService,
 	transportPtr *transport.TransportModule,
 	nodeIp string,
 	port int,
-	coordinatorToRaft chan protobuf.RaftPayload) {
+	coordinatorToRaft chan protobuf.RaftPayload){
 
 	rand.Seed(time.Now().UnixNano())
-	r := rand.Intn(MAX_TIME-MIN_TIME+1) + MIN_TIME
-	timeout := time.Duration(r) * time.Second
+	r:=rand.Intn(MAX_TIME - MIN_TIME + 1) + MIN_TIME
+	timeout:=time.Duration(r) * time.Second
 
-	R2RPort := port + 2
+	R2RPort:=port+2;
 
-	selfAddr := nodeIp
-	selfAddr = selfAddr + ":"
-	selfAddr += strconv.Itoa(R2RPort)
+	selfAddr:=nodeIp
+	selfAddr=selfAddr+":"
+	selfAddr+=strconv.Itoa(R2RPort)
 
-	raftStateMachine := RaftStateMachine(coordinatorToRaft, selfAddr, timeout)
-	raftStateMachine.gmsPtr = gmsPtr
-	raftStateMachine.transportPtr = transportPtr
+	raftStateMachine := RaftStateMachine(coordinatorToRaft,selfAddr,timeout)
+	raftStateMachine.gmsPtr=gmsPtr
+	raftStateMachine.transportPtr=transportPtr
 	raftStateMachine.startUpPromt()
-	raftStateMachine.voteRec = 0
-	raftStateMachine.voteTermMap = make(map[int32]int32)
-	raftStateMachine.termNum = 0
+	raftStateMachine.voteRec=0
+	raftStateMachine.voteTermMap= make(map[int32]int32)
+	raftStateMachine.termNum=0;
 	err := raftStateMachine.FSM.Event("init")
 	if err != nil {
 		fmt.Println(err)
 	}
-	go raftStateMachine.incomingMsgReader()
+	go raftStateMachine.incomingMsgReader();
 }
-
 //reference from
 //https://github.com/looplab/fsm/blob/v0.2.0/fsm.go#L88
-func RaftStateMachine(coordinatorToRaft chan protobuf.RaftPayload, node_ip_add string, _timeout time.Duration) *node {
+func RaftStateMachine(coordinatorToRaft chan protobuf.RaftPayload,node_ip_add string,_timeout time.Duration) *node {
 	currNode := &node{
-		node_ip:         node_ip_add,
-		timeout:         _timeout,
-		leader_ip:       "NULL",
-		fromCoordinator: coordinatorToRaft,
+		node_ip: node_ip_add,
+		timeout: _timeout,
+		leader_ip: "NULL",
+		fromCoordinator:coordinatorToRaft,
 	}
 
 	currNode.FSM = fsm.NewFSM(
@@ -128,23 +127,23 @@ func RaftStateMachine(coordinatorToRaft chan protobuf.RaftPayload, node_ip_add s
 			//
 			// 8. after_event - called after all events
 
-			"before_leader_to_follower": func(e *fsm.Event) {
+			"before_leader_to_follower":func(e *fsm.Event) {
 				CDprint.Println("LEADER STEPS DOWN")
 				defer color.Unset()
 				currNode.followerFunc(e)
 			},
-			"before_candidate_to_follower": func(e *fsm.Event) {
+			"before_candidate_to_follower":func(e *fsm.Event) {
 				CDprint.Println("EXISTING LEADER DETECTED")
 			},
-			"enter_follower": func(e *fsm.Event) {
+			"enter_follower":func(e *fsm.Event) {
 				defer color.Unset()
 				currNode.followerFunc(e)
 			},
-			"enter_candidate": func(e *fsm.Event) {
+			"enter_candidate":func(e *fsm.Event) {
 				defer color.Unset()
 				currNode.candidateFunc(e)
 			},
-			"enter_leader": func(e *fsm.Event) {
+			"enter_leader":func(e *fsm.Event) {
 				defer color.Unset()
 				currNode.leaderFunc(e)
 			},
@@ -152,6 +151,7 @@ func RaftStateMachine(coordinatorToRaft chan protobuf.RaftPayload, node_ip_add s
 				defer color.Unset()
 				currNode.enterState(e)
 			},
+
 		},
 	)
 
@@ -160,22 +160,22 @@ func RaftStateMachine(coordinatorToRaft chan protobuf.RaftPayload, node_ip_add s
 
 //MAIN STATE FUNCTIONS--------------------------------
 //startup promt
-func (this *node) startUpPromt() {
+func (this *node) startUpPromt(){
 	defer color.Unset()
-	Xprint.Println("[", this.node_ip, "]", " has initilized as a [follower] with timeout [", this.timeout, "]")
+	Xprint.Println("[", this.node_ip,"]"," has initilized as a [follower] with timeout [", this.timeout,"]")
 }
 
 // called after each state change
 func (this *node) enterState(fsm_event *fsm.Event) {
-	Xprint.Println("STATE TRANSITION [", this.node_ip, "]", fsm_event.Src, ">>", fsm_event.Dst)
+	Xprint.Println("STATE TRANSITION [", this.node_ip,"]",fsm_event.Src,">>",fsm_event.Dst)
 }
 
 // called after state has changed to follower
 func (this *node) followerFunc(fsm_event *fsm.Event) {
 	this.leaderMutex.Lock()
-	termNumber := this.termNum
+	termNumber:=this.termNum
 	this.leaderMutex.Unlock()
-	FWprint.Println("TERM >>[", termNumber, "] FOLLOWING [", this.leader_ip, "]")
+	FWprint.Println("TERM >>[",termNumber,"] FOLLOWING [", this.leader_ip,"]")
 	go this.timeoutToCandidate(fsm_event)
 }
 
@@ -191,132 +191,131 @@ func (this *node) candidateFunc(fsm_event *fsm.Event) {
 func (this *node) leaderFunc(fsm_event *fsm.Event) {
 	LDprint.Println("[LEADER NODE]")
 	LDprint.Println("[OUTBOUND]->PINGING")
-	for {
-		if this.FSM.Is("leader") {
+	for{
+		if(this.FSM.Is("leader")){
 			this.pingAllNodes(fsm_event)
-			ttSleep := MIN_TIME - NETWORK_DELAY
-			sleep_time := time.Duration(ttSleep) * time.Second
-			LDprint.Println(sleep_time, " TERM", this.termNum, ">>", this.FSM.Current())
+			ttSleep:=MIN_TIME-NETWORK_DELAY
+			sleep_time:=time.Duration(ttSleep)*time.Second
+			LDprint.Println(sleep_time," TERM",this.termNum,">>",this.FSM.Current())
 			time.Sleep(sleep_time)
-		} else {
+		} else{
 			LDprint.Println("[LEADER REVERT]")
-			break
+			break;
 		}
 	}
 }
-
 //----------------------------------------------------
 
 //HELPER FUNCTIONS------------------------------------
-func (this *node) timeoutToCandidate(fsm_event *fsm.Event) {
+func (this *node) timeoutToCandidate(fsm_event *fsm.Event){
 	this.timeoutMutex.Lock()
-	print_timeout := this.timeout
+	print_timeout:=this.timeout
 	this.timeoutMutex.Unlock()
-	FWprint.Println(this.node_ip, "timeout is", print_timeout)
-	for {
+	FWprint.Println(this.node_ip,"timeout is",print_timeout);
+	for{
 		this.timeoutMutex.Lock()
-		this.timeout -= REFRESH_RATE
+		this.timeout-=REFRESH_RATE
 		this.timeoutMutex.Unlock()
 		time.Sleep(REFRESH_RATE)
-		if this.timeout < 100*time.Millisecond {
-			break
+		if(this.timeout<100*time.Millisecond){
+			break;
 		}
 	}
 	// STATE TRANSITION INTO THE CANDIDATE STATE
 	this.FSM.Event("follower_to_candidate")
 }
-func (this *node) timeoutToFollower(fsm_event *fsm.Event) {
-	time.Sleep(10 * time.Second)
+func (this *node) timeoutToFollower(fsm_event *fsm.Event){
+	time.Sleep(10*time.Second)
 	this.reset()
 	// STATE TRANSITION INTO THE CANDIDATE STATE
-	if this.FSM.Is("candidate") {
+	if(this.FSM.Is("candidate")){
 		this.FSM.Event("candidate_to_follower")
 	}
 }
 
-func (this *node) incomingMsgReader() {
+func (this *node) incomingMsgReader(){
 	Xprint.Println("MSG READER INIT")
-	msg := protobuf.RaftPayload{}
-	for {
+	msg:=protobuf.RaftPayload{}
+	for{
 		select {
-		case msg = <-this.fromCoordinator:
-			if msg.Type == "PING" {
-				var newLeader = false
+		case msg=<-this.fromCoordinator:
+			if(msg.Type=="PING"){
+				var newLeader =false
 				this.reset()
-				if this.FSM.Is("candidate") {
+				if(this.FSM.Is("candidate")){
 					this.FSM.Event("candidate_to_follower")
-				} else if this.FSM.Is("leader") {
+				} else if(this.FSM.Is("leader")) {
 					this.FSM.Event("leader_to_follower")
 				}
 				this.leaderMutex.Lock()
-				this.termNum = msg.TermNum
-				if this.leader_ip == "NULL" {
-					this.leader_ip = msg.SenderIP
-					FWprint.Println("[INIT] FOLLOWING->", msg.SenderIP)
-				} else if this.leader_ip != msg.SenderIP {
-					this.leader_ip = msg.SenderIP
-					newLeader = true
-					FWprint.Println("FOLLOWING->", msg.SenderIP)
+				this.termNum=msg.TermNum;
+				if(this.leader_ip=="NULL"){
+					this.leader_ip=msg.SenderIP
+					FWprint.Println("[INIT] FOLLOWING->",msg.SenderIP)
+				} else if(this.leader_ip!=msg.SenderIP){
+					this.leader_ip=msg.SenderIP
+					newLeader=true;
+					FWprint.Println("FOLLOWING->",msg.SenderIP)
 				}
 				this.leaderMutex.Unlock()
-				if newLeader {
+				if(newLeader){
 					go this.setLeader(msg.SenderIP)
 				}
-			} else if msg.Type == "VOTEASK" {
+			}else if(msg.Type=="VOTEASK"){
 				//Xprint.Println("VOTE ASKED")
 				var response string
 				this.mapMutex.Lock()
-				_, exist := this.voteTermMap[msg.TermNum]
+				_ ,exist := this.voteTermMap[msg.TermNum]
 				if exist {
-					response = "NO"
+					response="NO"
 				} else {
-					response = "YES"
-					this.voteTermMap[msg.TermNum] = 1
+					response="YES"
+					this.voteTermMap[msg.TermNum]=1;
 				}
 				this.mapMutex.Unlock()
 
-				electionRes := &protobuf.RaftPayload{
-					Type:    "VOTERES",
-					VoteRes: response,
+				electionRes:=&protobuf.RaftPayload{
+					Type:                 "VOTERES",
+					VoteRes:              response,
 				}
-				marshalled_electionRes, err := proto.Marshal(electionRes)
-				if err != nil {
+				marshalled_electionRes, err :=proto.Marshal(electionRes)
+				if(err!=nil){
 					fmt.Println("[CRITIAL] CASTING ERROR")
 				}
-				this.transportPtr.R2RSend(marshalled_electionRes, msg.SenderIP)
-			} else if msg.Type == "VOTERES" {
+				this.transportPtr.R2RSend(marshalled_electionRes,msg.SenderIP)
+			} else if(msg.Type=="VOTERES"){
 				//CDprint.Println("VOTE RECIVED")
-				if msg.VoteRes == "YES" {
-					this.voteRec += 1
+				if(msg.VoteRes=="YES"){
+					this.voteRec+=1
 				}
-				if len(this.gmsPtr.GetAllNodes()) == this.voteRec {
+				if(len(this.gmsPtr.GetAllNodes())==this.voteRec){
 					this.FSM.Event("candidate_to_leader")
 				}
-			} else if msg.Type == "REVOLT" {
+			} else if(msg.Type=="REVOLT"){
 				RXprint.Println("REVOLT DETECTED")
-				if this.FSM.Is("leader") {
+				if(this.FSM.Is("leader")){
 					this.FSM.Event("leader_to_follower")
 				}
 			}
 		}
 	}
-	fmt.Println("CRITICAL ERROR, INBOUND COMMUNICATION HALTED")
+	fmt.Println("CRITICAL ERROR, INBOUND COMMUNICATION HALTED");
 }
 
-func (this *node) reset() {
+func (this *node) reset(){
 	rand.Seed(time.Now().UnixNano())
-	r := rand.Intn(MAX_TIME-MIN_TIME+1) + MIN_TIME
-	timeout := time.Duration(r) * time.Second
+	r:=rand.Intn(MAX_TIME - MIN_TIME + 1) + MIN_TIME
+	timeout:=time.Duration(r) * time.Second
 	this.timeoutMutex.Lock()
-	this.timeout = timeout
+	this.timeout=timeout
 	this.timeoutMutex.Unlock()
 }
 
-func (this *node) askForVotes(fsm_event *fsm.Event) {
+func (this *node) askForVotes(fsm_event *fsm.Event){
 	this.leaderMutex.Lock()
-	this.termNum++
+	this.termNum++;
 	this.leaderMutex.Unlock()
-	electionPage := &protobuf.RaftPayload{
+	electionPage:=&protobuf.RaftPayload{
 		Type:                 "VOTEASK",
 		SenderIP:             this.node_ip,
 		VoteRes:              "",
@@ -325,28 +324,28 @@ func (this *node) askForVotes(fsm_event *fsm.Event) {
 		Log:                  nil,
 	}
 
-	marshalled_electionPage, err := proto.Marshal(electionPage)
-	if err != nil {
+	marshalled_electionPage, err :=proto.Marshal(electionPage)
+	if(err!=nil){
 		fmt.Println("[CRITIAL] CASTING ERROR")
 	}
-	nodeLists := this.gmsPtr.GetAllNodes()
+	nodeLists:=this.gmsPtr.GetAllNodes()
 	var currPort string
-	this.voteRec = 1 // votes for yourself
+	this.voteRec=1 // votes for yourself
 
-	if len(nodeLists) == 1 {
+	if(len(nodeLists)==1){
 		this.FSM.Event("candidate_to_leader")
 	}
 	// then ask eveyone else ot vote for you
 	for i := 0; i < len(nodeLists); i++ {
-		currPort = parsePort(nodeLists[i])
-		if currPort != this.node_ip {
+		currPort=parsePort(nodeLists[i])
+		if(currPort!=this.node_ip){
 			//CDprint.Println("ASKING for VOTES FROM ",currPort)
-			this.transportPtr.R2RSend(marshalled_electionPage, currPort)
+			this.transportPtr.R2RSend(marshalled_electionPage,currPort)
 		}
 	}
 }
-func (this *node) pingAllNodes(fsm_event *fsm.Event) {
-	electionPage := &protobuf.RaftPayload{
+func (this *node) pingAllNodes(fsm_event *fsm.Event){
+	electionPage:=&protobuf.RaftPayload{
 		Type:                 "PING",
 		SenderIP:             this.node_ip,
 		VoteRes:              "",
@@ -355,56 +354,56 @@ func (this *node) pingAllNodes(fsm_event *fsm.Event) {
 		Log:                  nil,
 	}
 
-	marshalled_electionPage, err := proto.Marshal(electionPage)
-	if err != nil {
+	marshalled_electionPage, err :=proto.Marshal(electionPage)
+	if(err!=nil){
 		fmt.Println("[CRITIAL] CASTING ERROR")
 	}
-	nodeLists := this.gmsPtr.GetAllNodes()
+	nodeLists:=this.gmsPtr.GetAllNodes()
 	var currPort string
 	for i := 0; i < len(nodeLists); i++ {
-		currPort = parsePort(nodeLists[i])
-		if currPort != this.node_ip {
-			this.transportPtr.R2RSend(marshalled_electionPage, currPort)
+		currPort=parsePort(nodeLists[i])
+		if(currPort!=this.node_ip){
+			this.transportPtr.R2RSend(marshalled_electionPage,currPort)
 		}
 	}
 }
 
-func (this *node) setLeader(senderAddr string) {
-	time.Sleep(5 * time.Second)
-	var flag = false
+func(this *node) setLeader(senderAddr string){
+	time.Sleep(5*time.Second)
+	var flag =false;
 	this.leaderMutex.Lock()
-	if this.leader_ip != senderAddr {
-		flag = true
+	if(this.leader_ip!=senderAddr){
+		flag=true
 	}
 	this.leaderMutex.Unlock()
 
-	if flag {
-		RXprint.Println("ATTEMPTING TO ASSASSINATE THE CURRENT LEADER", senderAddr)
-		revolution := &protobuf.RaftPayload{
-			Type:     "REVOLT",
-			SenderIP: this.node_ip,
-			VoteRes:  "",
-			Log:      nil,
+	if(flag){
+		RXprint.Println("ATTEMPTING TO ASSASSINATE THE CURRENT LEADER",senderAddr)
+		revolution:=&protobuf.RaftPayload{
+			Type:                 "REVOLT",
+			SenderIP: 			  this.node_ip,
+			VoteRes:              "",
+			Log:                  nil,
 		}
 
-		marshalled_revolution, err := proto.Marshal(revolution)
-		if err != nil {
-			log.Println("[REVOLT ERR]", err)
+		marshalled_revolution, err :=proto.Marshal(revolution)
+		if(err != nil){
+			log.Println("[REVOLT ERR]",err)
 		}
-		this.transportPtr.R2RSend(marshalled_revolution, senderAddr)
+		this.transportPtr.R2RSend(marshalled_revolution,senderAddr)
 	}
 }
-func parsePort(address string) string {
+func parsePort(address string) string{
 	var port_num string
-	for i := 0; i < len(address); i++ {
-		if address[i] == ':' {
-			port_num = address[i+1:]
-			casted_port, err := strconv.Atoi(port_num)
-			if err != nil {
+	for i:=0;i< len(address);i++{
+		if(address[i]== ':'){
+			port_num=address[i+1:];
+			casted_port, err :=strconv.Atoi(port_num)
+			if(err!=nil){
 				fmt.Println("ERROR CASTING")
 			}
-			casted_port += 2
-			new_address := address[:i+1] + strconv.Itoa(casted_port)
+			casted_port+=2;
+			new_address:=address[:i+1]+strconv.Itoa(casted_port)
 			//fmt.Println("NEW ADDRESS->",new_address)
 			return new_address
 		}
